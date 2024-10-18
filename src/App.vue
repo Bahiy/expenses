552bd7fde67f87bae2419bf15a7b3e61b6d0a5a6
Bahiy <bahiy14@gmail.com>
2024-10-18 01:12:10 -0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+    <base-spinner />
+    <button @click="mostrarSpinner()">Mostrar Spinner</button>
+    <!-- <router-view /> -->
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import BaseSpinner from "./components/global/BaseSpinner.vue";
+export default {
+  name: "App",
 
-nav {
-  padding: 30px;
+  components: {
+    BaseSpinner,
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  methods: {
+    mostrarSpinner() {
+      this.$root.$emit("Spinner::show");
+    },
+  },
+};
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style></style>
