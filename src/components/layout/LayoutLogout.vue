6b33d711d3c1139cc587d680f741fab32094d676
Bahiy <bahiy14@gmail.com>
2024-10-24 13:29:41 -0300

    Add logout structure

---
+++ src/components/layout/LayoutLogout.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="btn btn-logout" @click="logout()">
+    <i class="fa fa-power-off"></i>
+    Sair
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      this.$root.$emit("Spinner::show");
+      await this.$auth.signOut();
+      this.$router.push({ name: "login" }).catch(() => {});
+      this.$root.$emit("Spinner::hide");
+      console.log();
+
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.btn-logout {
+  width: 100%;
+  cursor: pointer;
+  color: var(--light);
+  &:hover {
+    color: var(--featured);
+  }
+}
+</style>
--- src/components/layout/LayoutNavigation.vue
+++ src/components/layout/LayoutNavigation.vue
@@ -1,21 +1,29 @@
 <template>
-  <nav class="nav flex-column gap-2">
-    <router-link
-      class="nav-link"
-      exact
-      exact-active-class="active"
-      v-for="(router, i) in routerLinks"
-      :key="i"
-      :to="{ name: router.name }"
-    >
-      <i class="fa" :class="`fa-${router.meta.icon}`" />
-      {{ router.meta.title }}
-    </router-link>
+  <nav class="nav flex-column gap-2 justify-content-between">
+    <div>
+      <router-link
+        class="nav-link"
+        exact
+        exact-active-class="active"
+        v-for="(router, i) in routerLinks"
+        :key="i"
+        :to="{ name: router.name }"
+      >
+        <i class="fa" :class="`fa-${router.meta.icon}`" />
+        {{ router.meta.title }}
+      </router-link>
+    </div>
+
+    <layout-logout />
   </nav>
 </template>
 
 <script>
+import LayoutLogout from "./LayoutLogout";
 export default {
+  components: {
+    LayoutLogout,
+  },
   computed: {
     routerLinks() {
       return this.$router.options.routes.filter((r) => r.name !== "login");
