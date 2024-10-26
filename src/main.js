04bcf96e5284add45bc98102edc43b3296d01503
Your Name <bahiy14@gmail.com>
2024-10-26 19:25:33 -0300

    Add moneyMask and vue devtools

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/scss/app.scss";
 import FirebasePlugin from "./firebase/index"; // Caminho para o seu plugin
+import VueTheMask from "vue-the-mask";
+import money from "v-money";
+
+Vue.config.devtools = true;
 
 Vue.config.productionTip = false;
 Vue.use(FirebasePlugin);
+Vue.use(VueTheMask);
+Vue.use(money, {
+  decimal: ",",
+  thousands: ".",
+  prefix: "R$ ",
+  precision: 2,
+});
 
 new Vue({
   router,
